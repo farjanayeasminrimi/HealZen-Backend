4bb32bc6e8523a255d3cfe3f0199b1c1e1b3bad9
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-05-21 15:19:26 +0600

    created doctors and departments collection

--- index.js
+++ index.js
@@ -25,6 +25,12 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const database = client.db("HealZen");
+
+    //  doctors collection
+    const doctorsCollection = database.collection("doctors");
+    const departmentsCollection = database.collection("departments");
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
